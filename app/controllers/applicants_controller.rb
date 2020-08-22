b3735f9ff6638da7c45672f23e7e4105a140ca24
aurelien dhellemme <dhellemme.a@gmail.com>
2020-08-22 19:32:54 +0200

    newpj

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,2 +1,9 @@
 class ApplicantsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index]
+  def index
+    
+  end
+  def show
+    @applicant = Applicant.find(params[:id])
+  end
 end
