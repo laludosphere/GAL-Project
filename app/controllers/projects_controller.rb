1c4831ffafd2cc8173d7a92a67c317019bfe1819
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2020-08-22 13:43:44 +0200

    controller project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,39 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_params(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to project_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :code, :teammate, :description)
+  end
 end
